0fa06b1de847e2e33f2f2a698c6575530d17c446
wonedd <vinmarv@hotmail.com>
2023-09-28 20:09:30 -0300

    wip: server side rendering

--- src/infra/routes.js
+++ src/infra/routes.js
@@ -1,3 +1,7 @@
+import { promises as fsPromises } from 'fs';
+import path from 'path';
+import { URL, fileURLToPath } from 'url';
+
 import { Router } from 'express';
 import { SearchController } from '../controllers/SearchController.js'
 import { AuthController } from '../controllers/security/AuthController.js';
@@ -7,6 +11,12 @@ import { AuthService } from '../services/security/AuthService.js';
 import { SearchService } from '../services/SearchService.js';
 import { BotService } from '../services/security/BotService.js';
 import { BotController } from '../controllers/security/BotController.js';
+
+const { readFile } = fsPromises;
+
+const currentFileURL = new URL(import.meta.url);
+const currentDirectory = path.dirname(fileURLToPath(currentFileURL));
+
 
 const router = Router();
 
@@ -40,6 +50,20 @@ router.post('/search', (req, res) => {
 
 router.get('/', (req, res) => {
   searchController.fetchAll(req, res);
+});
+
+router.get('/teste', async (req, res) => {
+  const publicDir = path.join(currentDirectory, '../public');
+  const filePath = path.join(publicDir, 'index.html');
+  try {
+    const data = await readFile(filePath, 'utf8');
+
+    res.setHeader('Content-Type', 'text/html');
+    res.send(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erro ao ler o arquivo HTML.');
+  }
 });
 
 export function initRoutes(app) {
